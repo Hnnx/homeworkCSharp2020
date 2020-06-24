6d46da6509786165c1c67f4248d6a0f414e5bf82
HNNX <nejc.stibilj01@gmail.com>
2020-06-25 00:16:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homeworkCSharp2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("trying to re-learn C# D:!");
+        }
+    }
+}
