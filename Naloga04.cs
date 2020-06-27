188fd12e6f9e4dff3ab898468eb19de90b52042d
Nejc <nejc.stibilj01@gmail.com>
2020-06-27 21:57:07 +0200

    Naloga 4 done (y)

--- Naloga04.cs
+++ Naloga04.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,17 +8,43 @@ namespace homeworkCSharp2020
     class nal4
     {
 
-        public static void func(int starost)
+        public static void func(string letnica)
         {
 
-            if(starost > 9999 || starost < 1000)
+            if(letnica.Length != 4)
             {
-                Console.WriteLine("Please insert 4 digits");
-
-
-                
-
+                Console.WriteLine("Please insert a valid year of birth");
             }
+            else
+            {
+                try
+                {
+                    int currentYear = DateTime.Now.Year;
+                    int age = currentYear - (Convert.ToInt32(letnica));
+
+                    if(age == 17)
+                    {
+                        Console.WriteLine("Sorry, you're too young({0}) - you can vote next year", age);
+                    }
+                    else if(age < 18)
+                    {
+                        int difference = 18 - age;
+                        Console.WriteLine("Sorry, you're too young({0}) - you can vote in {1} years",age, difference );
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are {0} years old and are eligible to vote", age);
+                    }
+
+                   
+                }
+                catch
+                {
+                    Console.WriteLine("Your input cannot be converted to a number - please use a valid input (e.g 1980)");
+                }
+            }
+
+
 
         }
     }
--- Run.cs
+++ Run.cs
@@ -11,6 +11,7 @@ namespace homeworkCSharp2020
 
         static void Main(string[] args)
         {
+
             Console.WriteLine("Naloga 1:");
             Console.WriteLine("Napisi program, ki bo izracunal in izpisal rezultata racunov:\n"+
             "1) (10 + 5) / 3 = ?\n"+
@@ -32,6 +33,12 @@ namespace homeworkCSharp2020
             Console.WriteLine("Program naj sprejme 1 argument v obliki celega stevila. Podan argument naj bo 4 mestno stevilo, ki predstavlja letnico rojstva.\n" +
                 "Program naj izpise: \"Stari ste XX let.Lahko se udelezite volitev.\",\nce je starost 18 ali vec, ce pa je manj pa naj izpise: \"Zal ste premladi.Volitev se lahko udelezite cez XX let.\n" +
                 "Na mestu XX naj se izpisejo primerne stevilske vrednosti glede na vneseno letnico.");
+
+            nal4.func("1990");
+            nal4.func("2019");
+            nal4.func("2020");
+            nal4.func("2002");
+            nal4.func("2003");
 
 
 
