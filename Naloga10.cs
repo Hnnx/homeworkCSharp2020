7fce2fe4089fe1f6451078904a152ebe23b7adea
Nejc <nejc.stibilj01@gmail.com>
2020-06-29 22:52:39 +0200

    +Naloga 10, 11, 12

--- Naloga10.cs
+++ Naloga10.cs
@@ -10,6 +10,30 @@ namespace homeworkCSharp2020
         public static void func(string userInput)
         {
 
+            int tCount, aCount;
+            tCount = aCount = 0;
+
+            double tPerc, aPerc;
+            tPerc = aPerc = 0.0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+
+                if ( Char.ToUpper(userInput[i]) == 'T'){
+                    tCount++;
+                }
+                
+                if ( Char.ToUpper(userInput[i]) == 'A'){
+                    aCount++;
+                }
+
+            }
+
+            tPerc = (double)tCount / (double)userInput.Length * 100;
+            aPerc = (double)aCount / (double)userInput.Length * 100;
+
+            Console.WriteLine("Letter t is repeated {0} times. Percentage: {1}%", tCount, tPerc);
+            Console.WriteLine("Letter a is repeated {0} times. Percentage: {1}%", aCount, aPerc);
         }
     }
 }
