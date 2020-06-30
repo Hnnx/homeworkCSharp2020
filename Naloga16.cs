935b51511e73418be6bd89edd218212889eb56d3
Nejc <nejc.stibilj01@gmail.com>
2020-06-30 22:41:09 +0200

    Start Naloga16

---
+++ Naloga16.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeworkCSharp2020
+{
+    class nal16
+    {
+
+        public static void func()
+        {
+            Console.WriteLine("Insert first number:");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insert secind number:");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+
+            int[,] myArray = new int[firstNum, secondNum];
+
+        }
+    }
+}
