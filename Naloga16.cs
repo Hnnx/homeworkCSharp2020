8caf1e796e4881a29cda4083c5e521c605de556b
Nejc <nejc.stibilj01@gmail.com>
2020-07-01 22:39:46 +0200

    Debugging naloga 16

--- Naloga16.cs
+++ Naloga16.cs
@@ -9,14 +9,69 @@ namespace homeworkCSharp2020
 
         public static void func()
         {
-            Console.WriteLine("Insert first number:");
-            int firstNum = Convert.ToInt32(Console.ReadLine());
+            //DEFINE ARRAY SIZE
+            Console.WriteLine("Insert array X size:");
+            int arrayX = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert array Y size:");
+            int arrayY = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Insert secind number:");
-            int secondNum = Convert.ToInt32(Console.ReadLine());
+            //DECLARE MULTI ARR
+            int[,] multiArr = new int[arrayX, arrayY];
+
+            //DECALRE VAR TO STORE TOTAL
+            int total = 0;
+
+            //DECLARE VAR TO STORE TOTAL X/Y AXIS
+            int totalX = 0;
+            int totalY = 0;
 
 
-            int[,] myArray = new int[firstNum, secondNum];
+
+
+            for (int i = 0; i < multiArr.GetLength(0); i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < multiArr.GetLength(1); j++)
+                {
+                    //FILL WITH RANDOM 1-170
+                    Random rdm = new Random();
+                    multiArr[i, j] = rdm.Next(1, 5);
+
+
+                    //DISPLAY TABLE
+                    Console.Write(multiArr[i, j] + " ");
+
+
+                    //COUNT ALL
+                    total += multiArr[i, j];
+
+
+                }
+            }
+                    Console.WriteLine("\nTotal: {0}",total);
+
+
+
+            for (int i = 0; i < multiArr.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < multiArr.GetLength(1); j++)
+                {
+
+                    totalX += multiArr[0,j];
+                    totalY += multiArr[i,0];
+
+                }
+            }
+
+            Console.WriteLine("Total x: {0}", totalX);
+            Console.WriteLine("Total y: {0}", totalY);
+
+
+
+
+
+
 
         }
     }
