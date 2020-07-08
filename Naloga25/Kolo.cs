bcd02529569ee3f760b42cf12b0d06df5b6a30bf
Nejc <nejc.stibilj01@gmail.com>
2020-07-08 23:38:34 +0200

    + start naloga 25 debugging naloga 24

---
+++ Naloga25/Kolo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeworkCSharp2020.Naloga25
+{
+    class Kolo
+    {
+
+        public static void testF()
+        {
+
+        Console.WriteLine("testFolder");
+
+        }
+
+
+    }
+
+
+}
