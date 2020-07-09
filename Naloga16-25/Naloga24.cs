a359773f4be9681d11436540ba1fe8f81258b166
HNNX <nejc.stibilj01@gmail.com>
2020-07-09 20:14:23 +0200

    debugging naloga 24 :C

--- Naloga16-25/Naloga24.cs
+++ Naloga16-25/Naloga24.cs
@@ -11,22 +11,67 @@ namespace homeworkCSharp2020
         public static void readBigFile()
         {
 
+            //Read File, Lines (array) to String, Split to Array, Convert to INT array
+            string path = @"C:\out\st.txt";
+            string deljivaPath = @"C:\out\deljiva.txt";
+            string primePath = @"C:\out\prime.txt";
+            string[] readNumFile = File.ReadAllLines(path);
+            string arrToStr = String.Concat(readNumFile);
+            string[] stringNumz = arrToStr.Split(";");
+            int total = 0;
 
-            string path = @"F:\out\st.txt";
-            string[] numz = File.ReadAllLines(path);
+            int[] intNumz = new int[stringNumz.Length];
 
-            //string path2 = @"F:\out\st2.txt";
-
-
-            foreach (var num in numz)
+            for (int i = 0; i < intNumz.Length; i++)
             {
+                //Convert to INT
+                intNumz[i] = Convert.ToInt32(stringNumz[i]);
 
 
+                //Pokni v Deljiva.txt
+                if (intNumz[i] % 3 == 0 && intNumz[i] % 5 == 0 && intNumz[i] % 11 == 0)
+                {
+                    File.AppendAllText(deljivaPath, intNumz[i].ToString());
 
+                }
+
+                //Izpisi total v terminal
+                total += intNumz[i];
+
+                /*
+                //Pokni v prime.txt
+                if (!File.Exists(primePath))
+                {
+                if (isPrime(intNumz[i]))
+                {
+                File.AppendAllText(primePath, intNumz[i].ToString() + Environment.NewLine);
+                }
+                }
+
+                */
             }
+
+            int avg = total / intNumz.Length;
+            Console.WriteLine("Average vseh stevilk je: {0}", avg);
 
         }
 
+
+        private static Boolean isPrime(int n)
+        {
+            int counter = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter == 2;
+
+        }
 
     }
 }
